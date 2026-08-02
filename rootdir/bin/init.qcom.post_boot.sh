5c561633586aa73c60a7b16f9e18c053bc4f519a
PabloG <gpablo1@yahoo.com.ar>
2026-08-02 18:57:33 +0200

    Force use of lz4

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -837,9 +837,9 @@ function configure_zram_parameters() {
         let zRamSizeMB=4096
     fi
 
-    if [ "$low_ram" == "true" ]; then
-        echo lz4 > /sys/block/zram0/comp_algorithm
-    fi
+	if [ -f /sys/block/zram0/comp_algorithm ]; then
+	    echo lz4 > /sys/block/zram0/comp_algorithm
+	fi
 
     if [ -f /sys/block/zram0/disksize ]; then
         disksize=`cat /sys/block/zram0/disksize`
